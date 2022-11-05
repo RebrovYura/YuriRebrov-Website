84fb8e56df0a7af62a31bde0470e1851c43e2663
Yuri Rebrov <yurirebrov01@gmail.com>
2022-11-06 01:00:23 +0300

    fix some bugs

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
     function getThemeFromLocalStorage(parentSelector) {
         if (localStorage.getItem('theme')) {
             parentSelector.classList.add(localStorage.getItem('theme'));
+            themeToggle.classList.add(localStorage.getItem('icon'));
         }
     }
 
@@ -19,9 +20,8 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             parent.classList.add('dark');
             localStorage.setItem('theme', 'dark');
+            localStorage.setItem('icon', 'bxs-sun');
             themeToggle.classList.replace('bxs-moon', 'bxs-sun');
         }
-        console.log('hello');
     });
-
 });
